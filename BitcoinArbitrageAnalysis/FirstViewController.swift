276e5874a71f7cd59fc292ee0036be9074b6057e
Adam Heimendinger <adamhei@mac-air-01.local>
2018-01-28 11:32:19 -0600

    Initial Commit

---
+++ BitcoinArbitrageAnalysis/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BitcoinArbitrageAnalysis
+//
+//  Created by Adam Heimendinger on 1/28/18.
+//  Copyright © 2018 Adam Heimendinger. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
